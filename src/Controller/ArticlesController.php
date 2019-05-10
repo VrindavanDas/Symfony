d167e629cb5e9773a71ec6049601af43a27ad0fa
Victor <noblevic@gmail.com>
2019-05-10 11:06:15 +0300

    add form and add entries in DB

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -4,16 +4,20 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
 
 class ArticlesController extends AbstractController
 {
     /**
      * @Route("/articles", name="articles")
      */
-    public function index()
+    public function index(ArticleRepository $articleRepository)
     {
-        return $this->render('articles/index.html.twig', [
-            'controller_name' => 'ArticlesController',
-        ]);
+        $articles = $articleRepository->findAll();
+
+        dump($articles);
+
+        return $this->render('articles/index.html.twig', compact ('articles'));
     }
 }
--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -6,6 +6,7 @@ namespace App\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+
 class HomePageController extends AbstractController
 {
     /**
@@ -13,7 +14,6 @@ class HomePageController extends AbstractController
      * */
     public function home () {
         $testdata = ['test'=>'Home'];
-
 
         return $this->render('home.html.twig', $testdata);
     }
